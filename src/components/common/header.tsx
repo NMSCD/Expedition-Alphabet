c66a3f12c2e501a91c380b502c30c226fb753123
Kurt Lourens <kurt.lourens@entelect.co.za>
2021-05-18 00:18:11 +0200

    better call to action

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { HashLink } from 'react-router-hash-link';
 
+import * as route from '../../constants/route';
+import { BasicInternalLink } from '../../components/core/link';
+
 interface IProps {
 }
 
@@ -8,11 +11,13 @@ export const Header: React.FC<IProps> = (props: IProps) => {
     return (
         <section id="header">
             <div className="inner">
-                <span className="icon solid major fa-cloud"></span>
                 <h1>No Man's Sky<br />Expedition alphabet</h1>
                 <p>Is there a secret alphabet in NMS?<br />Is HelloGames giving us pieces of the puzzle?</p>
+                <ul className="special mt2">
+                    <li><BasicInternalLink href={route.alphabet} additionalClassNames="primary button scrolly">Skip straight to alphabet</BasicInternalLink></li>
+                </ul>
                 <ul className="actions special">
-                    <li><HashLink to="#whatStarted" className="button scrolly">Lets find out!</HashLink></li>
+                    <li><HashLink to="#whatStarted" className="primary button scrolly">Read More</HashLink></li>
                 </ul>
             </div>
         </section>
