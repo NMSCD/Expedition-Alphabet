47bb80f6b61c3c2a08376f39a63e2f4142fddaa1
Kurt Lourens <kurt.lourens@entelect.co.za>
2021-05-18 00:11:10 +0200

    fix hash nav

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { HashLink } from 'react-router-hash-link';
 
 interface IProps {
 }
@@ -11,7 +12,7 @@ export const Header: React.FC<IProps> = (props: IProps) => {
                 <h1>No Man's Sky<br />Expedition alphabet</h1>
                 <p>Is there a secret alphabet in NMS?<br />Is HelloGames giving us pieces of the puzzle?</p>
                 <ul className="actions special">
-                    <li><a href="#whatStarted" className="button scrolly">Lets find out!</a></li>
+                    <li><HashLink to="#whatStarted" className="button scrolly">Lets find out!</HashLink></li>
                 </ul>
             </div>
         </section>
